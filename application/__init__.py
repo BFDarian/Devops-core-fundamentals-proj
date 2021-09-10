1f62b9d213adeec34679fe519eb6ac369730f19b
Ubuntu <ubuntu@ip-172-31-45-27.eu-west-1.compute.internal>
2021-09-10 13:13:20 +0000

    update __init__

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dataTeam.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "secret_key"
 
